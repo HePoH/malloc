e3e40fcaa8140979d9236c98caa2120303c622b0
Dmitry Medvedev <demon0900@mail.ru>
2017-06-27 13:23:38 +0600

    release

--- include/mem_struct.h
+++ include/mem_struct.h
@@ -16,21 +16,6 @@ typedef struct mcs {
         struct mcs* mp_prev;
 } MEM_CHUNK_STRUCT;
 
-/*typedef struct mcl {
-        MEM_CHUNK_STRUCT chunk;
-
-        struct fil *next;
-        struct fil *prev;
-} MEM_CHUNK_LIST;
-
-        1. 0 - 10
-        2. 11 - 100
-        3. 101 - 1024
-        4. 1025 - 10240
-        5. > 10240
-
-MEM_CHUNK_LIST mem_alloc_list[5];*/
-
 MEM_CHUNK_STRUCT *mem_head = NULL, *mem_tail = NULL;
 int *hlb = NULL, *hub = NULL;
 
